e7d04339cf35779f84213f4fdab8f06ac41c3ccd
widya Yasa <widyarusmananda15@gmail.com>
2024-10-13 14:13:31 +0800

    feat: add download qr image

--- resources/views/dashboard/rooms/show.blade.php
+++ resources/views/dashboard/rooms/show.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Detail Ruangan')
 @section('main')
     <div class="table-wrapper mt-[20px] input-teacher">
-        <form method="post" class="grid grid-cols-12 gap-4" enctype="multipart/form-data">
+        <div class="grid grid-cols-12 gap-4">
             <div class="col-span-12 flex flex-col">
                 <label for="name" class="text-second mb-1">Nama</label>
                 <input disabled type="text" class="input-crud" name="name" id="name" value="{{ old('name', $room->name) }}"
@@ -47,14 +47,15 @@
                 <label for="image" class="d-block mb-3">
                     @if ( asset('storage/qrcodes/room-' . $room->id . '.png'))
                         <img src="{{  asset('storage/qrcodes/room-' . $room->id . '.png') }}"
-                             class="preview-img border" alt="">
+                             class="preview-img border" alt="" id="qrImage">
                     @else
                         <img src="{{ asset('assets/img/upload-image.jpg') }}" class="preview-img border"
                              width="300" alt="">
                     @endif
                 </label>
+                <button type="button" class="button btn-main" id="downloadBtn">Download Qr</button>
             </div>
-        </form>
+        </div>
         <div class="col-span-12 flex items-center gap-3 mt-5">
             <a href="{{ route('rooms.index') }}" class="button btn-second text-white" type="reset">Kembali</a>
         </div>
@@ -65,6 +66,23 @@
         previewImg("input", "preview-img")
         $("#status").select2()
         $("#room_category_id").select2()
+
+        document.getElementById('downloadBtn').addEventListener('click', () => {
+            const imgElement = document.getElementById('qrImage');
+            const imgUrl = imgElement.src;
+            console.log(imgUrl)
+            const downloadImage = (url, fileName) => {
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = fileName;
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+            };
+
+            downloadImage(imgUrl, 'Qr Code Ruangan {{$room->name}}.png');
+        });
+
     </script>
 @endpush
 
